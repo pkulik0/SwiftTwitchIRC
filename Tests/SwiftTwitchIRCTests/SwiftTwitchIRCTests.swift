89330d029470900f6a1b3f7911541e237e31cc4e
pkulik0 <pkulik0@icloud.com>
2022-07-22 21:08:53 +0200

    Tests: remove onHostStarted, add printChatMsg

--- Tests/SwiftTwitchIRCTests/SwiftTwitchIRCTests.swift
+++ Tests/SwiftTwitchIRCTests/SwiftTwitchIRCTests.swift
@@ -5,21 +5,23 @@ final class SwiftTwitchIRCTests: XCTestCase {
     func testExample() throws {
         let expectation = XCTestExpectation(description: "aha")
         let irc = SwiftTwitchIRC(username: "qurrie", token: "3184l994nsn2lgpq8gaup3oe3xifty",
-                                 onMessageReceived: printMsg,
+                                 onMessageReceived: printChatMsg,
                                  onWhisperReceived: printMsg,
                                  onNoticeReceived: printMsg,
                                  onUserEvent: printMsg,
                                  onUserStateChanged: printMsg,
                                  onRoomStateChanged: printMsg,
                                  onClearChat: printMsg,
-                                 onClearMessage: printMsg,
-                                 onHostStarted: printMsg)
+                                 onClearMessage: printMsg)
         irc.joinChannel(channel: "hasanabi")
 
         wait(for: [expectation], timeout: 360000.0)
     }
     
     func printMsg<T>(msg: T) {
-        print(msg)
+//        print(msg)
+    }
+    func printChatMsg(msg: SwiftTwitchIRC.ChatMessage) {
+//        print("(\(msg.chatroom)) \(msg.userName): \(msg.text)")
     }
 }

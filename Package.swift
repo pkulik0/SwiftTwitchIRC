b89c4615845db04179aea2cb10831f6e2c4a73b1
pkulik0 <pkulik0@icloud.com>
2022-09-25 17:56:15 +0200

    Remove unused files

--- Package.swift
+++ Package.swift
@@ -21,8 +21,5 @@ let package = Package(
         .target(
             name: "SwiftTwitchIRC",
             dependencies: []),
-        .testTarget(
-            name: "SwiftTwitchIRCTests",
-            dependencies: ["SwiftTwitchIRC"]),
     ]
 )
